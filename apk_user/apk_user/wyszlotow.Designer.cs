2a74a527cce3b5c2a640e864daf8baee89dc6ca0
mikolop1 <gintermikolaj@gmail.com>
2023-01-15 10:58:19 +0100

    10:58 15.01.2023

--- apk_user/apk_user/wyszlotow.Designer.cs
+++ apk_user/apk_user/wyszlotow.Designer.cs
@@ -44,7 +44,10 @@
             this.fromdate = new System.Windows.Forms.DateTimePicker();
             this.dokadbox = new System.Windows.Forms.ComboBox();
             this.skadbox = new System.Windows.Forms.ComboBox();
+            this.button1 = new System.Windows.Forms.Button();
             this.podsumowanie1 = new apk_user.podsumowanie();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
             ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
             this.panel1.SuspendLayout();
             this.panel2.SuspendLayout();
@@ -89,6 +92,9 @@
             // 
             this.panel1.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
             | System.Windows.Forms.AnchorStyles.Right)));
+            this.panel1.Controls.Add(this.label4);
+            this.panel1.Controls.Add(this.label5);
+            this.panel1.Controls.Add(this.button1);
             this.panel1.Controls.Add(this.panel2);
             this.panel1.Controls.Add(this.label3);
             this.panel1.Controls.Add(this.label2);
@@ -144,7 +150,7 @@
             // label3
             // 
             this.label3.Font = new System.Drawing.Font("Nirmala UI", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label3.Location = new System.Drawing.Point(120, 55);
+            this.label3.Location = new System.Drawing.Point(40, 55);
             this.label3.Name = "label3";
             this.label3.Size = new System.Drawing.Size(41, 25);
             this.label3.TabIndex = 9;
@@ -155,7 +161,7 @@
             // label2
             // 
             this.label2.Font = new System.Drawing.Font("Nirmala UI", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label2.Location = new System.Drawing.Point(117, 18);
+            this.label2.Location = new System.Drawing.Point(37, 18);
             this.label2.Name = "label2";
             this.label2.Size = new System.Drawing.Size(44, 25);
             this.label2.TabIndex = 8;
@@ -213,7 +219,7 @@
             // 
             this.dokadbox.Font = new System.Drawing.Font("Nirmala UI", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.dokadbox.FormattingEnabled = true;
-            this.dokadbox.Location = new System.Drawing.Point(161, 55);
+            this.dokadbox.Location = new System.Drawing.Point(81, 55);
             this.dokadbox.Margin = new System.Windows.Forms.Padding(2);
             this.dokadbox.Name = "dokadbox";
             this.dokadbox.Size = new System.Drawing.Size(120, 25);
@@ -224,12 +230,29 @@
             // 
             this.skadbox.Font = new System.Drawing.Font("Nirmala UI", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.skadbox.FormattingEnabled = true;
-            this.skadbox.Location = new System.Drawing.Point(161, 18);
+            this.skadbox.Location = new System.Drawing.Point(81, 18);
             this.skadbox.Margin = new System.Windows.Forms.Padding(2);
             this.skadbox.Name = "skadbox";
             this.skadbox.Size = new System.Drawing.Size(120, 25);
             this.skadbox.TabIndex = 0;
             this.skadbox.SelectedIndexChanged += new System.EventHandler(this.skadbox_SelectedIndexChanged);
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.Color.Transparent;
+            this.button1.FlatAppearance.BorderSize = 0;
+            this.button1.FlatAppearance.MouseDownBackColor = System.Drawing.Color.Transparent;
+            this.button1.FlatAppearance.MouseOverBackColor = System.Drawing.Color.Transparent;
+            this.button1.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button1.Font = new System.Drawing.Font("Nirmala UI", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.Location = new System.Drawing.Point(671, 18);
+            this.button1.Margin = new System.Windows.Forms.Padding(2);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(94, 62);
+            this.button1.TabIndex = 6;
+            this.button1.Text = "Show all";
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click_1);
             // 
             // podsumowanie1
             // 
@@ -239,6 +262,26 @@
             this.podsumowanie1.Size = new System.Drawing.Size(417, 228);
             this.podsumowanie1.TabIndex = 4;
             this.podsumowanie1.Visible = false;
+            // 
+            // label4
+            // 
+            this.label4.Font = new System.Drawing.Font("Nirmala UI", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(209, 54);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(72, 25);
+            this.label4.TabIndex = 12;
+            this.label4.Text = "Date to:";
+            this.label4.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
+            // 
+            // label5
+            // 
+            this.label5.Font = new System.Drawing.Font("Nirmala UI", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.Location = new System.Drawing.Point(206, 17);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(75, 25);
+            this.label5.TabIndex = 11;
+            this.label5.Text = "Date from:";
+            this.label5.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
             // 
             // wyszlotow
             // 
@@ -275,5 +318,8 @@
         private System.Windows.Forms.Panel panel2;
         private System.Windows.Forms.PictureBox pictureBox1;
         private podsumowanie podsumowanie1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
     }
 }
